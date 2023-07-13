728117a7dda35ab9c6d99a3af7395570884de329
barmoshe <1barmoshe1@gmail.com>
2023-07-13 15:34:06 +0300

    Add project files.

---
+++ webSiteAndServer/Pages/SignInPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webSiteAndServer.Pages
+{
+    public class SignInPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
