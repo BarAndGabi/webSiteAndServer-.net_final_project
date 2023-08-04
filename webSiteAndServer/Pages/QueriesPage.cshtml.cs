eb9f07be292a6aafe31231c1ad4e65ed7d368028
Gabriel Lempert <gabrielempert@gmail.com>
2023-08-04 15:37:42 +0300

    QuerryPage

--- webSiteAndServer/Pages/QueriesPage.cshtml.cs
+++ webSiteAndServer/Pages/QueriesPage.cshtml.cs
@@ -25,24 +25,102 @@ namespace webSiteAndServer.Pages
 
         public IActionResult OnPost(string queryName)
         {
-            if (queryName == "query1")
+            switch (queryName)
             {
-                Users = this.connect4Context.users.ToList();
+                case "query1":
+                    ViewData["SelectedQuery"] = 1;
+                    Query1();
+                    break;
+                case "query2":
+                    Query2();
+                      break;
+                case "querry3":
+                    break;
+                case "querry4":
+                    break;
+                case "querry5":
+                    break;
+                case "querry6":
+                    break;
+                case "querry7":
+                    break;
 
-                QueryResult = new DataTable();
-                QueryResult.Columns.Add("PlayerId", typeof(int));
-                QueryResult.Columns.Add("Firstname", typeof(string));
-                QueryResult.Columns.Add("PhoneNumber", typeof(string));
-                QueryResult.Columns.Add("Country", typeof(string));
-
-                foreach (var user in Users)
-                {
-                    QueryResult.Rows.Add(user.PlayerId, user.FirstName, user.PhoneNumber, user.Country);
-                }
-                ViewData["QueryResult"] = QueryResult;
+                // Add cases for other queries
+                default:
+                    break;
             }
 
             return Page();
         }
+
+        private void Query1()
+        {
+            Users = this.connect4Context.Users.ToList();
+
+            QueryResult = new DataTable();
+            QueryResult.Columns.Add("PlayerId", typeof(int));
+            QueryResult.Columns.Add("Firstname", typeof(string));
+            QueryResult.Columns.Add("PhoneNumber", typeof(string));
+            QueryResult.Columns.Add("Country", typeof(string));
+
+            foreach (var user in Users)
+            {
+                QueryResult.Rows.Add(user.PlayerId, user.FirstName, user.PhoneNumber, user.Country);
+            }
+            ViewData["QueryResult"] = QueryResult;
+        }
+
+        private void Query2()
+        {
+            var userGames = this.connect4Context.Games
+                .GroupBy(g => g.PlayerId)
+                .Select(group => new
+                {
+                    PlayerId = group.Key,
+                    LatestStartTime = group.Max(g => g.StartTime)
+                })
+                .OrderBy(item => item.PlayerId)
+                .ToList();
+
+            var users = this.connect4Context.Users.ToList();
+
+            QueryResult = new DataTable();
+            QueryResult.Columns.Add("Username", typeof(string));
+            QueryResult.Columns.Add("LatestGameStartTime", typeof(DateTime));
+
+            foreach (var userGame in userGames)
+            {
+                var user = users.FirstOrDefault(u => u.PlayerId == userGame.PlayerId);
+                if (user != null)
+                {
+                    QueryResult.Rows.Add(user.FirstName, userGame.LatestStartTime);
+                }
+            }
+
+            ViewData["QueryResult"] = QueryResult;
+            
+        }
+
+        private void Query3()
+        {
+            var games = this.connect4Context.Games.ToList();
+
+            QueryResult = new DataTable();
+            QueryResult.Columns.Add("Id", typeof(string));
+            QueryResult.Columns.Add("PlayerId", typeof(int));
+            QueryResult.Columns.Add("PlayerWon", typeof(bool));
+            QueryResult.Columns.Add("GameFinished", typeof(bool));
+            QueryResult.Columns.Add("StartTime", typeof(DateTime));
+            QueryResult.Columns.Add("TimePlayedSeconds", typeof(int));
+
+            foreach (var game in games)
+            {
+                QueryResult.Rows.Add(game.Id, game.PlayerId, game.PlayerWon, game.GameFinished, game.StartTime, game.TimePlayedSeconds);
+            }
+
+            ViewData["QueryResult"] = QueryResult;
+        }
+
     }
+
 }
