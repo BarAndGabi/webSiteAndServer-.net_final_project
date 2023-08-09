70a12cc1b9747e094c4de5e342f4f29ef88f731b
Gabriel Lempert <gabrielempert@gmail.com>
2023-08-09 10:58:46 +0300

    DELETE AND EDIT V2

--- webSiteAndServer/Pages/UserManagement.cshtml.cs
+++ webSiteAndServer/Pages/UserManagement.cshtml.cs
@@ -3,113 +3,89 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
 using webSiteAndServer.Data;
 using webSiteAndServer.Model;
-using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 public class UserManagementModel : PageModel
 {
-    private readonly Connect4Context connect4context;
+    private readonly Connect4Context connect4Context;
 
     public UserManagementModel(Connect4Context context)
     {
-        this.connect4context = context;
+        this.connect4Context = context;
     }
+    [BindProperty]
+    public string ColumnBeingEdited { get; set; }
 
     [BindProperty]
-    [Required(ErrorMessage = "New Value is required.")]
-    public string NewValue { get; set; }
+    public int UserIdBeingEdited { get; set; }
 
     public List<User> Users { get; set; }
 
     public void OnGet()
     {
-        Users = this.connect4context.users.ToList();
+        Users = connect4Context.users.ToList();
     }
 
-    public bool ValidateNewValue(string columnName)
+    public IActionResult OnPost()
     {
-        if (columnName == "PlayerId")
-        {
-            bool playerIdExists = connect4context.users.Any(u => u.PlayerId == int.Parse(NewValue));
-            if (playerIdExists)
-            {
-                return false; // PlayerId already exists
-            }
-        }
-        else if (columnName == "PhoneNumber")
-        {
-            if (NewValue.Length < 10)
-            {
-                return false;
-            }
-        }
+        int userId = int.Parse(Request.Form["userId"]);
+        string action = Request.Form["action"];
 
-        // Add more validation checks for other columns if needed
-
-        return true; // Validation passed
-    }
-
-    public IActionResult OnPostEdit(int userId, string column)
-    {
-        var user = this.connect4context.users.FirstOrDefault(u => u.PlayerId == userId);
+        var user = this.connect4Context.users.FirstOrDefault(u => u.PlayerId == userId);
 
         if (user != null)
         {
-            if (!ModelState.IsValid)
+            if (action == "edit")
             {
-                Users = this.connect4context.users.ToList();
-                return Page();
-            }
+                ColumnBeingEdited = Request.Form["column"];
+                UserIdBeingEdited = userId;
 
-            if (!ValidateNewValue(column))
-            {
-                ModelState.AddModelError("NewValue", "Invalid new value.");
-                Users = this.connect4context.users.ToList();
-                return Page();
-            }
-
-            if (column == "PlayerId")
-            {
-                bool playerIdExists = connect4context.users.Any(u => u.PlayerId == int.Parse(NewValue));
-                if (playerIdExists)
+                if (ColumnBeingEdited == "Country")
                 {
-                    ModelState.AddModelError("NewValue", "Player ID already exists.");
-                    Users = this.connect4context.users.ToList();
-                    return Page();
+                    // Handle editing country
+                    // No need to perform any action here
                 }
-            }
-            else if (column == "PhoneNumber")
-            {
-                if (NewValue.Length < 10)
+                else if (ColumnBeingEdited == "OtherColumn")
                 {
-                    ModelState.AddModelError("NewValue", "Phone Number must have at least 10 digits.");
-                    Users = this.connect4context.users.ToList();
-                    return Page();
+                    // Handle editing other column
+                    string newValue = Request.Form["newValue"];
+
+                    if (ColumnBeingEdited == "PlayerId")
+                    {
+                        // Validate if the new ID is available
+                        int newId = int.Parse(newValue);
+                        var existingUser = this.connect4Context.users.FirstOrDefault(u => u.PlayerId == newId);
+                        if (existingUser != null)
+                        {
+                            // ID already taken, handle the error
+                            // You might want to show an error message or redirect back to the page
+                            return Page();
+                        }
+                        user.PlayerId = newId;
+                    }
+                    else if (ColumnBeingEdited == "FirstName")
+                    {
+                        user.FirstName = newValue;
+                    }
+                    else if (ColumnBeingEdited == "PhoneNumber")
+                    {
+                        user.PhoneNumber = newValue;
+                    }
                 }
-            }
 
-            // Proceed with updating the database
-            if (column == "PlayerId")
+                this.connect4Context.SaveChanges();
+            }
+            else if (action == "editCountry")
             {
-                user.PlayerId = int.Parse(NewValue);
+                string newCountryValue = Request.Form["newCountryValue"];
+                user.Country = newCountryValue;
+                this.connect4Context.SaveChanges();
             }
-            // else if other columns...
-
-            connect4context.SaveChanges();
-        }
-
-        return RedirectToPage();
-    }
-
-    public IActionResult OnPostDelete(int userId)
-    {
-        var user = this.connect4context.users.FirstOrDefault(u => u.PlayerId == userId);
-
-        if (user != null)
-        {
-            this.connect4context.users.Remove(user);
-            this.connect4context.SaveChanges();
+            else if (action == "delete")
+            {
+                this.connect4Context.users.Remove(user);
+                this.connect4Context.SaveChanges();
+            }
         }
 
         return RedirectToPage();
