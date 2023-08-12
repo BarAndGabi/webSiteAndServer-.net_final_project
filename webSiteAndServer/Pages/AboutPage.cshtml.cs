ce46598ae71f9d820037b44904ac0e19b058fdb4
barmoshe <1barmoshe1@gmail.com>
2023-08-12 10:43:37 +0300

    about page

---
+++ webSiteAndServer/Pages/AboutPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webSiteAndServer.Pages
+{
+    public class AboutPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
